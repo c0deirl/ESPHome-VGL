85b786d6175c4c0cbddf4bcbfc2ca86012964b08
Matthew Greathouse <greathouse.matthew@gmail.com>
2025-09-10 08:57:43 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LVGLWidgetDesigner from './lvgl-widget-designer';
+
+function App() {
+  return (
+    <div className="App">
+      <LVGLWidgetDesigner />
+    </div>
+  );
+}
+
+export default App;
